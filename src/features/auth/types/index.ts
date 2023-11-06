26271f5e20a289033d3ccfd1a7928d35d14023a3
Infamous0192 <infamous0192@gmail.com>
2023-11-06 09:39:57 +0800

    chore: improve type

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -1,9 +1,3 @@
-export type Creds = {
-  name: string;
-  phone: string;
-  email: string;
-  role: 'Superadmin' | 'Admin';
-  agency: string;
-  is_from_arkan: number;
-  display_picture: string;
-};
+import { UserAdmin, UserCustomer } from '@/features/user';
+
+export type Creds = UserAdmin | UserCustomer;
