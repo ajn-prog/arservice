1d5b8d986551d0cdd4079c964df472050aedf0c7
Infamous0192 <infamous0192@gmail.com>
2023-11-13 06:53:21 +0800

    fix: correct hook usage

--- src/features/user/components/EngineerSelect.tsx
+++ src/features/user/components/EngineerSelect.tsx
@@ -1,12 +1,12 @@
 import { MultiSelect, MultiSelectProps } from '@mantine/core';
 import { useMemo } from 'react';
 
-import { useBiodata } from '../api';
+import { useEngineers } from '../api';
 
 type Props = Omit<MultiSelectProps, 'data'>;
 
 export const EngineerSelect: React.FC<Props> = ({ ...props }) => {
-  const { data } = useBiodata({ params: { limit: 0 } });
+  const { data } = useEngineers({ params: { limit: 0 } });
 
   const engineers = useMemo(() => {
     if (!data) return [];
