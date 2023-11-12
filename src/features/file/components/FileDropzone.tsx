9909ccfec561df96f2e25aae70624e500f27b59a
Infamous0192 <infamous0192@gmail.com>
2023-11-13 04:38:14 +0800

    style: improve dropzone

--- src/features/file/components/FileDropzone.tsx
+++ src/features/file/components/FileDropzone.tsx
@@ -21,27 +21,29 @@ export const FileDropzone: React.FC<Props> = ({ value, label, error, ...props })
         }}
         {...props}
       >
-        <Group justify="center" gap="xl" mih={120} style={{ pointerEvents: 'none' }}>
+        <Group justify="center" gap="lg" mih={120} style={{ pointerEvents: 'none' }}>
           <Dropzone.Accept>
             <IconUpload
               style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
-              stroke={1.5}
+              stroke={1}
             />
           </Dropzone.Accept>
           <Dropzone.Reject>
             <IconX
               style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
-              stroke={1.5}
+              stroke={1}
             />
           </Dropzone.Reject>
           <Dropzone.Idle>
             <IconPhoto
               style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
-              stroke={1.5}
+              stroke={1}
             />
           </Dropzone.Idle>
 
-          {value ? <div>{value.name}</div> : <div>Klik atau drag file untuk upload</div>}
+          <div className="text-gray-600">
+            {value ? <span>{value.name}</span> : <span>Klik atau drag file untuk upload</span>}
+          </div>
         </Group>
       </Dropzone>
       {error && <div className="text-xs text-red-600 mt-1.5">{error}</div>}
