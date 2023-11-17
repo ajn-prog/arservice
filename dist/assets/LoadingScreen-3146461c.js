b472fd001089823988f70eca5c05747d0b5a5d4d
Infamous0192 <infamous0192@gmail.com>
2023-11-18 03:28:54 +0800

    build: update dist

---
+++ dist/assets/LoadingScreen-3146461c.js
@@ -0,0 +1,1 @@
+import{j as s,aK as a,av as e}from"./index-36612c22.js";const n=()=>s.jsx(a,{className:"w-full h-full",children:s.jsx(e,{})});export{n as L};
--- dist/assets/LoadingScreen-34b7b5f0.js
+++
@@ -1,1 +0,0 @@
-import{j as s,aJ as a,at as e}from"./index-1a1c1bd9.js";const t=()=>s.jsx(a,{className:"w-full h-full",children:s.jsx(e,{})});export{t as L};
