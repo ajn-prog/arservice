63cff2e4a1620e52f3c9d76f98962d47998a70df
Infamous0192 <infamous0192@gmail.com>
2023-12-01 15:10:47 +0800

    build: update build files

---
+++ dist/assets/getBiodata-33f92825.js
@@ -0,0 +1,1 @@
+import{G as e,a as t}from"./index-74fbf4f3.js";async function r(){return(await t.get("/ar-service/profile/biodata")).data.data}function i({config:a}={}){return e({...a,queryKey:["biodata"],queryFn:()=>r()})}export{i as u};
