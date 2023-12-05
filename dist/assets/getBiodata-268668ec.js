b52ec990893fe6718717a79040a9f7ed7ac09ab0
Infamous0192 <infamous0192@gmail.com>
2023-12-05 14:16:12 +0800

    refactor: merge production

--- dist/assets/getBiodata-268668ec.js
+++
@@ -1,1 +0,0 @@
-import{v as e,a as t}from"./index-a6533a44.js";async function r(){return(await t.get("/ar-service/profile/biodata")).data.data}function i({config:a}={}){return e({...a,queryKey:["biodata"],queryFn:()=>r()})}export{i as u};
