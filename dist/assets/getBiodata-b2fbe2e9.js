c643681bcb1075224d11bfe44d90db6e02e3319a
Dwa Meizadewa <infamous0192@gmail.com>
2024-01-04 08:38:01 +0800

    build: update build files

--- dist/assets/getBiodata-b2fbe2e9.js
+++
@@ -1,1 +0,0 @@
-import{e,a as t}from"./index-e06ce023.js";async function r(){return(await t.get("/ar-service/profile/biodata")).data.data}function i({config:a}={}){return e({...a,queryKey:["biodata"],queryFn:()=>r()})}export{i as u};
