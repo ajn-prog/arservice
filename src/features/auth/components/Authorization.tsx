2069d323ef9f28432f61e7032ea0259e7c943f70
Dwa Meizadewa <infamous0192@gmail.com>
2024-01-09 11:58:47 +0800

    chore: improve authorization

--- src/features/auth/components/Authorization.tsx
+++ src/features/auth/components/Authorization.tsx
@@ -1,14 +1,40 @@
-import { UserRole } from '@/features/user';
-
 import { useAuth } from '../hooks';
 
-type Props = {
-  role: Array<UserRole | `-${UserRole}`>;
-  children: React.ReactNode;
-};
+type UserRole =
+  | 'Sales'
+  | 'ASM'
+  | 'Logistic'
+  | 'Engineer'
+  | 'Direktur'
+  | 'Admin_Legal'
+  | 'Superadmin'
+  | 'Finance'
+  | 'Partner_Director'
+  | 'Partner_ASM'
+  | 'Partner_Sales'
+  | 'Customer';
+
+type Props =
+  | { role: Array<UserRole>; children: React.ReactNode }
+  | { role: Array<`-${UserRole}`>; children: React.ReactNode };
 
 export const Authorization: React.FC<Props> = ({ role, children }) => {
-  const { isPermitted } = useAuth();
+  const { creds } = useAuth();
+
+  function isPermitted(roles: Array<UserRole | `-${UserRole}`>) {
+    const exceptedRoles = roles.filter((role) => role.startsWith('-')).map((role) => role.slice(1));
+    const isExcepted = exceptedRoles.some((role) => creds?.role === role);
+
+    if (exceptedRoles.length > 0) {
+      return !isExcepted;
+    }
+
+    const acceptedRoles = roles.filter((role) => !role.startsWith('-'));
+
+    const isAccepted = acceptedRoles.some((role) => creds?.role === role);
+
+    return isAccepted;
+  }
 
   if (!isPermitted(role)) return null;
 
