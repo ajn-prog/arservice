4f7c007c22ca9dedb32e012700720a6607264562
Dwa Meizadewa <infamous0192@gmail.com>
2024-05-02 15:47:37 +0800

    build: update dist

--- dist/assets/IconPaperclip-483b73bc.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./index-9ab8ff68.js";var e=a("paperclip","IconPaperclip",[["path",{d:"M15 7l-6.5 6.5a1.5 1.5 0 0 0 3 3l6.5 -6.5a3 3 0 0 0 -6 -6l-6.5 6.5a4.5 4.5 0 0 0 9 9l6.5 -6.5",key:"svg-0"}]]);export{e as I};
--- dist/assets/getBiodata-f72352cf.js
+++
@@ -1,1 +0,0 @@
-import{e,a as t}from"./index-9ab8ff68.js";async function r(){return(await t.get("/ar-service/profile/biodata")).data.data}function i({config:a}={}){return e({...a,queryKey:["biodata"],queryFn:()=>r()})}export{i as u};
